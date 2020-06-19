11f2ec8293fa51484c9666a7b4e7a5eb4c0ea837
jayesh-srivastava <jayesh0200@gmail.com>
2020-06-19 22:51:36 +0530

    committed new versions of layouts

--- app/src/main/java/com/example/android/scorekeeper/SplashActivity.java
+++ app/src/main/java/com/example/android/scorekeeper/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.example.android.scorekeeper;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -10,7 +12,7 @@ public class SplashActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splashscreen);
+        setContentView(R.layout.splash_layout);
 
         new Handler().postDelayed(new Runnable() {
 
@@ -18,7 +20,7 @@ public class SplashActivity extends AppCompatActivity
             @Override
             public void run() {
                 // This method will be executed once the timer is over
-                Intent i = new Intent(SplashActivity.this, FullscreenActivity.class);
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(i);
                 finish();
             }
